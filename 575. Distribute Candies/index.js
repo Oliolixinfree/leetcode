2f8b47d8adf4bec560b6e574c3d3a5c50bfcb650
Oliolixinfree <offlane@tut.by>
2026-04-01 15:15:46 +0300

    575. Distribute Candies

---
+++ 575. Distribute Candies/index.js
@@ -0,0 +1,9 @@
+const distributeCandies = function (candyType) {
+	const uniq = new Set(candyType)
+
+	return candyType.length / 2 >= uniq.size ? uniq.size : candyType.length / 2
+}
+
+console.log(distributeCandies([1, 1, 2, 2, 3, 3]))
+console.log(distributeCandies([1, 1, 2, 3]))
+console.log(distributeCandies([6, 6, 6, 6]))
