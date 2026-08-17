a57a83205c935ad9261fb9c5464647cb13297237
Oliolixinfree <vanish8ii@gmail.com>
2026-08-17 16:24:17 +0300

    3658. GCD of Odd and Even Sums

---
+++ 3658. GCD of Odd and Even Sums/index.js
@@ -0,0 +1,11 @@
+function gcdOfOddEvenSums(n) {
+  return Math.abs((n * n) - (n * (n + 1)))
+};
+
+// or
+// function gcdOfOddEvenSums(n) {
+//   return n;
+// }
+
+
+console.log(gcdOfOddEvenSums(4))
